9a0acd8dd48f9b3fb6551b97380961a3ac429159
fancn21th <fancn21th@gmail.com>
2022-10-08 16:47:12 +0800

    WTF is ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-const world = "world";
+class Foo {
+  titles: string[] | undefined;
 
-function hello(who: string = world): string {
-  return `Hello ${who}! `;
+  constructor() {}
 }
 
-console.log(hello());
+const foo = new Foo();
+
+// try to remove the guard on the left
+foo.titles && foo.titles.forEach((t) => console.log(t));

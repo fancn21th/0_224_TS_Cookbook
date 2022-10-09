d95908ef59657d302c4f23750e4c8fb5fe01560f
fancn21th <fancn21th@gmail.com>
2022-10-09 11:09:48 +0800

    optional

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 interface User {
   name: string;
   age: number;
+  favorite?: string;
 }
 
+//  use case : redux state must be immutable
+// +remove optional property
 type ReadOnlyUser = {
-  readonly [K in keyof User]: User[K];
+  readonly [K in keyof User]-?: User[K];
 };
 
 const looseUser: User = { name: "alex", age: 18 };
 const strictUser: ReadOnlyUser = { name: "bob", age: 20 };
 
 looseUser.age = 21;
-strictUser.age = 23;
+// strictUser.age = 23;

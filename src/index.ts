44fe1867dc76effb979747c2f0c926db538c1ff0
fancn21th <fancn21th@gmail.com>
2022-10-17 15:20:10 +0800

    generic on object

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,55 @@
-interface User {
-  name: string;
-  age: number;
+interface ConfigObj<TRoute extends string> {
+  routes: TRoute[];
+  fetchers: {
+    [K in TRoute]: () => any;
+  };
 }
 
-type ReadOnlyUser = {
-  readonly [K in keyof User]: User[K];
+type RouteType = "/" | "/about" | "/contact";
+
+/**
+ *   - it is a clear duplication that RouteType and routes declared in configObj are equals in terms of values
+ *   - or we could say RouteType is hard coded
+ *
+ */
+
+const configObj: ConfigObj<RouteType> = {
+  routes: ["/", "/about", "/contact"],
+
+  fetchers: {
+    "/": () => {
+      return {};
+    },
+    "/about": () => {
+      return {};
+    },
+    "/contact": () => {
+      return {};
+    },
+  },
 };
 
-const looseUser: User = { name: "alex", age: 18 };
-const strictUser: ReadOnlyUser = { name: "bob", age: 20 };
+/**
+ *   - [X] identity function
+ *
+ */
 
-looseUser.age = 21;
-strictUser.age = 23;
+const generateConfigObject = <TRoute extends string>(
+  config: ConfigObj<TRoute>
+) => config;
+
+const configObj2 = generateConfigObject({
+  routes: ["/", "/about", "/contact"],
+
+  fetchers: {
+    "/": () => {
+      return {};
+    },
+    "/about": () => {
+      return {};
+    },
+    "/contact": () => {
+      return {};
+    },
+  },
+});
